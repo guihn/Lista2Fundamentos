431aabe8a0c8b38dce5fc36e3fa5c3db92e80dd8
Guilherme Henrique Ninov <guilhermehn@protonmail.com>
2023-03-29 16:26:19 -0300

    Atualizado Ex09

--- exe09Guilherme.c
+++ exe09Guilherme.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
-//não funciona 
-int main(){
-	int dia, mes;
 
-	printf("Informe o dia:");
-	scanf("%d", &dia);
+int main() {
+  int dia, mes;
+  char *signo;
 
-	printf("Informe o mes:");
-	scanf("%d", &mes);
+  printf("Digite o dia de nascimento: ");
+  scanf("%d", &dia);
+  printf("Digite o mês de nascimento (em numero): ");
+  scanf("%d", &mes);
 
-	if(dia>=22 && dia<=20 && mes>12 &&mes<=1) printf("Signo: Capricornio\n");
-	if(dia>=21 && dia<=19 && mes>1 &&mes<=2) printf("Signo: Aquario\n");
-	if(dia>=20 && dia<=20 && mes>2 &&mes<=3) printf("Signo: Peixes\n");
-	if(dia>=21 && dia<=20 && mes>3 &&mes<=4) printf("Signo: Aries\n");
-	if(dia>=21 && dia<=20 && mes>4 &&mes<=5) printf("Signo: Touro\n");
-	if(dia>=21 && dia<=20 && mes>5 &&mes<=6) printf("Signo: Gemeos\n");
-	if(dia>=21 && dia<=21 && mes>6 &&mes<=7) printf("Signo: Cancer\n");
-	if(dia>=22 && dia<=22 && mes>7 &&mes<=8) printf("Signo: Leao\n");
-	if(dia>=23 && dia<=22 && mes>8 &&mes<=9) printf("Signo: Virgem\n");
-	if(dia>=23 && dia<=21 && mes>9 &&mes<=10) printf("Signo: Libra\n");
-	if(dia>=23 && dia<=21 && mes>10 &&mes<=11) printf("Signo: Escorpiao\n");
-	if(dia>=21 && dia<=22 && mes>11 &&mes<=12) printf("Signo: Sagitario\n");
-	
-	
-	return 0;
+  if ((mes == 1 && dia >= 22) || (mes == 2 && dia <= 19)) signo = "Aquario";
+  else if ((mes == 2 && dia >= 20) || (mes == 3 && dia <= 20)) signo = "Peixes";
+  else if ((mes == 3 && dia >= 21) || (mes == 4 && dia <= 20)) signo = "Aries";
+  else if ((mes == 4 && dia >= 21) || (mes == 5 && dia <= 20)) signo = "Touro";
+  else if ((mes == 5 && dia >= 21) || (mes == 6 && dia <= 20)) signo = "Gemeos";
+  else if ((mes == 6 && dia >= 21) || (mes == 7 && dia <= 21)) signo = "Cancer";
+  else if ((mes == 7 && dia >= 22) || (mes == 8 && dia <= 22)) signo = "Leao";
+  else if ((mes == 8 && dia >= 23) || (mes == 9 && dia <= 22)) signo = "Virgem";
+  else if ((mes == 9 && dia >= 23) || (mes == 10 && dia <= 22)) signo = "Libra";
+  else if ((mes == 10 && dia >= 23) || (mes == 11 && dia <= 21)) signo = "Escorpiao";
+  else if ((mes == 11 && dia >= 22) || (mes == 12 && dia <= 21)) signo = "Sagitario";
+  else signo = "Capricórnio";
+
+  printf("Signo: %s\n", signo);
+
+  return 0;
 }
